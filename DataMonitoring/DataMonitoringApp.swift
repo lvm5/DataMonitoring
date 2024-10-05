2785ec5cf7859829069615766f5c22780c959818
Leandro Vansan <59117400+lvm5@users.noreply.github.com>
2024-10-05 08:10:52 -0300

    Initial Commit

---
+++ DataMonitoring/DataMonitoringApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataMonitoringApp.swift
+//  DataMonitoring
+//
+//  Created by Leandro Morais on 05-10-2024.
+//
+
+import SwiftUI
+
+@main
+struct DataMonitoringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
